5361428ea99e983569e41f6101aa22842547865c
Cekepek <ckpk7171@gmail.com>
2023-05-04 17:34:32 +0700

    fragments

---
+++ app/src/main/java/com/cekepek/ubayakuliner160420021/view/ReservasiFragment.kt
@@ -0,0 +1,19 @@
+package com.cekepek.ubayakuliner160420021.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cekepek.ubayakuliner160420021.R
+
+class ReservasiFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reservasi, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/cekepek/ubayakuliner160420021/view/ReviewFragment.kt
@@ -0,0 +1,20 @@
+package com.cekepek.ubayakuliner160420021.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cekepek.ubayakuliner160420021.R
+
+class ReviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_review, container, false)
+    }
+
+}
